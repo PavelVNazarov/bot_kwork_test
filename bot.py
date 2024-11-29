e47f8002432e98f5ff5211d1c84504aa11fc2ccc
PavelVNazarov <19pavel76@mail.ru>
2024-11-29 13:32:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,15 @@
 # bot.py
-import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN
-from handlers import start, check_subscription, exercise_selection
-
-# Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+from handlers import start_handler, exercise_handler
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("check_subscription", check_subscription))
-    dp.add_handler(CommandHandler("exercise_selection", exercise_selection))
+    dp.add_handler(CommandHandler("start", start_handler))
+    dp.add_handler(CommandHandler("exercise", exercise_handler))
 
     updater.start_polling()
     updater.idle()
